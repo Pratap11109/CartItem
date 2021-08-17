90cf9d38114f6b5609804784f8b6cc40dfae02e6
Pratap Thakur <Pratap.r.thakur1999@gmail.com>
2021-08-17 11:16:55 +0530

    Single Source of Truth 'Delete,Increment,Decrement Methode added in Cart.jsx file '

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -2,13 +2,62 @@ import React, { Component } from 'react'
 import CartItem from './CartItem'
 
 class Cart extends Component {
-    state = {}
+    state = {
+        arr: [
+            {
+                id: 1,
+                price: 1999,
+                title: "Mobile",
+                qty: 1
+            },
+            {
+                id: 2,
+                price: 3999,
+                title: "LapTop",
+                qty: 1
+            }, {
+                id: 3,
+                price: 99,
+                title: "Watch",
+                qty: 1
+            }
+        ]
+    }
+
+    incrmentQty = obj => {
+        console.log(obj);
+        // const arr = [...this.state.arr];
+        let index = this.state.arr.indexOf(obj);
+        this.state.arr[index].qty += 1;
+        this.setState({
+            arr: this.state.arr
+        })
+    }
+
+    deleteItem = (obj) => {
+        const arr = this.state.arr.filter((n) => n !== obj);
+        this.setState({
+            arr
+        })
+    }
+
+    decrmentQty = obj => {
+        if (obj.qty === 1) return this.deleteItem(obj);
+        let index = this.state.arr.indexOf(obj);
+        this.state.arr[index].qty -= 1;
+        this.setState({
+            arr: this.state.arr
+        })
+    }
+
     render() {
+
         return (
             <div className="Cart-Container">
-                <CartItem />
-                <CartItem />
-                <CartItem />
+                { this.state.arr.map((obj) => {
+                    return <CartItem key={obj.id} obj={obj} onIncrment={this.incrmentQty}
+                        onDecrement={this.decrmentQty} onDelete={this.deleteItem} />
+                })}
             </div>
         );
     }
--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,32 +1,22 @@
 import React from "react";
 
 class CartItem extends React.Component {
-     state = {
-          qty: 1
-     };
-
-     increment = () => {
-          this.setState({ qty: this.state.qty + 1 })
-
-     }
-     decrment = () => {
-          this.setState({ qty: (this.state.qty === 0 ? 0 : this.state.qty - 1) })
-     }
+     
      render() {
-          
+          const {obj ,onDecrement,onIncrment,onDelete}=this.props;
           return (
                <div className="cart-item">
                     <div className="left-block">
                          <img style={styles.img} alt="Img Product" />
                     </div>
                     <div className="right-block">
-                         <div style={{ color: "red" }, styles.fontWeight}>Phone</div>
-                         <div style={{ color: "#770" }, styles.fontWeight}>Rs 999</div>
-                         <div style={{ color: "#770" }, styles.fontWeight}>qty {this.state.qty}</div>
+                         <div style={{ color: "red" }, styles.fontWeight}>{obj.title}</div>
+                         <div style={{ color: "#770" }, styles.fontWeight}>Rs {obj.price}</div>
+                         <div style={{ color: "#770" }, styles.fontWeight}>qty {obj.qty}</div>
                          <div className="cart-item-actions">
-                              <img className="action-icons" onClick={this.increment} alt="Increment" src="https://t4.ftcdn.net/jpg/01/26/10/59/240_F_126105961_6vHCTRX2cPOnQTBvx9OSAwRUapYTEmYA.jpg" />
-                              <img className="action-icons" onClick={this.decrment} alt="Decrement" src="https://t4.ftcdn.net/jpg/03/30/24/99/240_F_330249927_k8oy0p4zZqSAdxd1jxlhB0ZPT3fGLpjw.jpg" />
-                              <img className="action-icons" alt="Delete" src="https://t3.ftcdn.net/jpg/02/76/19/96/240_F_276199650_Hs5K3QnXm9ZMBLd3DT44YcdWUIf8GHxO.jpg" />
+                              <img className="action-icons" onClick={()=>onIncrment(obj)} alt="Increment" src="https://t4.ftcdn.net/jpg/01/26/10/59/240_F_126105961_6vHCTRX2cPOnQTBvx9OSAwRUapYTEmYA.jpg" />
+                              <img className="action-icons" onClick={()=>onDecrement(obj)} alt="Decrement" src="https://t4.ftcdn.net/jpg/03/30/24/99/240_F_330249927_k8oy0p4zZqSAdxd1jxlhB0ZPT3fGLpjw.jpg" />
+                              <img className="action-icons"  onClick={()=>onDelete(obj)} alt="Delete" src="https://t3.ftcdn.net/jpg/02/76/19/96/240_F_276199650_Hs5K3QnXm9ZMBLd3DT44YcdWUIf8GHxO.jpg" />
                          </div>
 
                     </div>
