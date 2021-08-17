edc3f3f7fca2997c55e2862788714766786a09d3
Pratap Thakur <Pratap.r.thakur1999@gmail.com>
2021-08-17 20:21:03 +0530

    all Function move to App.js from Cart.jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Cart from './Cart'
 
+class App extends Component {
+  state = {
+    arr: [
+      {
+        id: 1,
+        price: 1999,
+        title: "Mobile",
+        qty: 1
+      },
+      {
+        id: 2,
+        price: 3999,
+        title: "LapTop",
+        qty: 1
+      }, {
+        id: 3,
+        price: 99,
+        title: "Watch",
+        qty: 1
+      }
+    ]
+  }
 
-function App() {
-  return (
-    <div className="App">
-     <Cart/>
-    </div>
-  );
+  incrmentQty = obj => {
+    console.log(obj);
+    // const arr = [...this.state.arr];
+    let index = this.state.arr.indexOf(obj);
+    this.state.arr[index].qty += 1;
+    this.setState({
+      arr: this.state.arr
+    })
+  }
+
+  deleteItem = (obj) => {
+    const arr = this.state.arr.filter((n) => n !== obj);
+    this.setState({
+      arr
+    })
+  }
+
+  decrmentQty = obj => {
+    if (obj.qty === 1) return this.deleteItem(obj);
+    let index = this.state.arr.indexOf(obj);
+    this.state.arr[index].qty -= 1;
+    this.setState({
+      arr: this.state.arr
+    })
+  }
+
+  render() {
+    const { arr } = this.state;
+    return (
+      <div className="App">
+        < Cart arr={arr}
+          onIncrese={this.incrmentQty}
+          onDecrese={this.decrmentQty}
+          onDelete={this.deleteItem} />
+      </div>);
+  }
 }
 
 export default App;
